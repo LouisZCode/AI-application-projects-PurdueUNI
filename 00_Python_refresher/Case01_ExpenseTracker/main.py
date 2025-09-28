24515f0ef1e2c6df59fd4bfcccfeeeb3af4d0178
LouisZCode <lgzg90@hotmail.com>
2025-09-28 21:06:58 +0200

    Start: Read and add noted of details of Case =1 Expense Tracker

---
+++ 00_Python_refresher/Case01_ExpenseTracker/main.py
@@ -0,0 +1,22 @@
+def main():
+    print("Hello from expensetracker!")
+
+#Make an Expense Tracker that can:
+# -Categorize the Expense
+# -Set Monthly Budgets
+# -Save and Load Expense Data
+# -Menu driven Interface
+
+# Ideas:
+# Learn to use GRADIO
+
+# STEPS
+# TO-DO 1: Function to ask the user for the Expense Details
+# TO-DO 2: Function to restieve and display all stored expenses
+# TO-DO 3: Function that sets and tracks a monthly budget
+# TO-DO 4: Save and Load Expenses on CSV     Pandas*
+# TO-DO 5: Create an interactive Menu 
+
+
+if __name__ == "__main__":
+    main()
